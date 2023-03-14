09d77ca0f50bb6c0f50fe4c341caac6c37c3f7a4
Surganova Marina <masurganova@gmail.com>
2023-03-14 15:20:52 +0300

    test users for projects xstl

--- src/test/java/ru/javaops/masterjava/xml/util/XsltProcessorTest.java
+++ src/test/java/ru/javaops/masterjava/xml/util/XsltProcessorTest.java
@@ -4,6 +4,10 @@ import com.google.common.io.Resources;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.io.Writer;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class XsltProcessorTest {
     @Test
@@ -14,5 +18,20 @@ public class XsltProcessorTest {
             XsltProcessor processor = new XsltProcessor(xslInputStream);
             System.out.println(processor.transform(xmlInputStream));
         }
+    }
+
+    @Test
+    public void transformGroups() throws Exception {
+        try (InputStream xslInputStream = Resources.getResource("groups.xsl").openStream();
+             InputStream xmlInputStream = Resources.getResource("payload.xml").openStream()) {
+
+            XsltProcessor processor = new XsltProcessor(xslInputStream);
+            processor.setParameter("projectName", "topjava");
+           String html = processor.transform(xmlInputStream);
+            try (Writer writer = Files.newBufferedWriter(Paths.get("out/groups.html"))) {
+                writer.write(html);
+            }
+        }
+
     }
 }
