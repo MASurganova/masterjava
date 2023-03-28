0da5a8430e85207a42d577bfca7fa410600bcd43
Surganova Marina <masurganova@gmail.com>
2023-03-28 15:21:29 +0300

    unique email

--- persist/src/test/java/ru/javaops/masterjava/persist/dao/UserDaoTest.java
+++ persist/src/test/java/ru/javaops/masterjava/persist/dao/UserDaoTest.java
@@ -1,17 +1,17 @@
 package ru.javaops.masterjava.persist.dao;
 
-import com.google.common.collect.Comparators;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import ru.javaops.masterjava.persist.UserTestData;
 import ru.javaops.masterjava.persist.model.User;
+import ru.javaops.masterjava.persist.model.UserFlag;
 
-import java.util.Comparator;
 import java.util.List;
 
 import static ru.javaops.masterjava.persist.UserTestData.FIST5_USERS;
+import static ru.javaops.masterjava.persist.UserTestData.USER1;
 
 public class UserDaoTest extends AbstractDaoTest<UserDao> {
 
@@ -40,5 +40,13 @@ public class UserDaoTest extends AbstractDaoTest<UserDao> {
         dao.clean();
         dao.insertAll(FIST5_USERS, 100);
         Assert.assertEquals(FIST5_USERS.size(), dao.getWithLimit(100).size());
+    }
+
+    @Test
+    public void duplicateEmail() {
+        int size = dao.getWithLimit(Integer.MAX_VALUE).size();
+        User duplicate = new User("duplicate", USER1.getEmail(), UserFlag.active);
+        dao.insert(duplicate);
+        Assert.assertEquals(size, dao.getWithLimit(Integer.MAX_VALUE).size());
     }
 }
