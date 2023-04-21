eda613ba1eb1054be733f33e29f0f3a82fec0e1f
Surganova Marina <masurganova@gmail.com>
2023-04-21 12:54:20 +0300

    fix ProjectProcessor

--- web/upload/src/main/java/ru/javaops/masterjava/upload/ProjectProcessor.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/ProjectProcessor.java
@@ -1,33 +1,55 @@
 package ru.javaops.masterjava.upload;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import ru.javaops.masterjava.persist.DBIProvider;
 import ru.javaops.masterjava.persist.dao.GroupDao;
 import ru.javaops.masterjava.persist.dao.ProjectDao;
-import ru.javaops.masterjava.persist.model.City;
 import ru.javaops.masterjava.persist.model.Group;
 import ru.javaops.masterjava.persist.model.Project;
 import ru.javaops.masterjava.persist.model.type.GroupType;
+import ru.javaops.masterjava.xml.schema.ObjectFactory;
+import ru.javaops.masterjava.xml.schema.Payload;
+import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 
+import javax.xml.bind.JAXBException;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 public class ProjectProcessor {
     private final ProjectDao projectDao = DBIProvider.getDao(ProjectDao.class);
     private final GroupDao groupDao = DBIProvider.getDao(GroupDao.class);
+    private static final JaxbParser jaxbParser = new JaxbParser(ObjectFactory.class);
 
-    public Map<String, Group> process(StaxStreamProcessor processor) throws XMLStreamException {
-        while (processor.startElement("Project", "Projects")) {
-            Project project = new Project(processor.getAttribute("name"), processor.getElementValue("description"));
-            int projectId = projectDao.insertGeneratedId(project);
-            String element;
-            while (processor.startElement("Group", "Project")) {
-                Group newGroup = new Group(processor.getAttribute("name"), GroupType.valueOf(processor.getAttribute("type")), projectId);
-                groupDao.insert(newGroup);
+    public Map<String, Group> process(StaxStreamProcessor processor) throws XMLStreamException, JAXBException {
+        val unmarshaller = jaxbParser.createUnmarshaller();
+        val projectsMap = projectDao.getAsMap();
+        val groupsMap = groupDao.getAsMap();
+        List<Group> newGroups = new ArrayList<>();
+        processor.doUntil(XMLEvent.START_ELEMENT, "Projects");
+        Payload.Projects projects = unmarshaller.unmarshal(processor.getReader(), Payload.Projects.class);
+        projects.getProject().forEach(p -> {
+            Project project = projectsMap.get(p.getName());
+            if (project == null) {
+                project = new Project(p.getName(), p.getDescription());
+                log.info("Insert project " + project);
+                projectDao.insert(project);
             }
-        }
+            int projectId = project.getId();
+            p.getGroup().forEach(g -> {
+                if (!groupsMap.containsKey(g.getName())) {
+                    newGroups.add(new Group(g.getName(),
+                            GroupType.valueOf(g.getType().value()), projectId));
+                }
+            });
+        });
+        log.info("Insert groups: " + newGroups);
+        groupDao.insertBatch(newGroups);
         return groupDao.getAsMap();
     }
 }
